3955b4e2c579b0c5b03f837fef088f222a1efffb
vaziliybober <vaziliybober@gmail.com>
2021-05-08 10:01:25 +0300

    add makeAtomic to library interface

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import {
   toStringOperation,
 } from './operation.js';
 
+import { makeAtomic } from './atomic.js';
+
 export {
   makeOperation as make,
+  makeAtomic,
   applyOperation as apply,
   transformOperations as transform,
   composeOperations as compose,
